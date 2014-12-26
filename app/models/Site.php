3801d762cb8e79bb68d38bdf7748c188dcd24ac1
samika <samika@kaarina.org>
2014-12-26 11:38:38 +0200

    Add Site model

---
+++ app/models/Site.php
@@ -0,0 +1,19 @@
+<?php
+
+class Site extends Eloquent {
+
+	/**
+	 * The database table used by the model.
+	 *
+	 * @var string
+	 */
+	protected $table = 'site';
+
+	/**
+	 * The attributes excluded from the model's JSON form.
+	 *
+	 * @var array
+	 */
+	protected $hidden = [];
+
+}
