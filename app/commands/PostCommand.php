efa95ae85ef2700511242134788d118a8d2a6ae8
samika <samika@kaarina.org>
2015-01-04 00:26:24 +0200

    Bugfix queuing in right order

--- app/commands/PostCommand.php
+++ app/commands/PostCommand.php
@@ -46,6 +46,7 @@ class PostCommand extends Command {
 		$posts = Post::whereNotNull('url')
 			->where('nextCheckAt', '<', $updateDt)
 			->take(30)
+			->orderBy('nextCheckAt')
 			->get();
 
 		print $posts->count() . ' Entries will be added to queue' . PHP_EOL;
